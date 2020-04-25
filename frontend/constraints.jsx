7bbea51b487e4272661cc4632568a953185dd830
Mike Pennisi <mike@mikepennisi.com>
2020-04-24 21:15:46 -0400

    Correct handling of shift dates

--- frontend/constraints.jsx
+++ frontend/constraints.jsx
@@ -6,6 +6,26 @@ import {
 	Input,
 } from '@airtable/blocks/ui';
 
+/**
+ * Remove time information from an ISO-8601-formatted string so that it may be
+ * used as the value of an HTML "date" input element.
+ *
+ * @param {string} date
+ *
+ * @returns {string}
+ */
+const trimDate = (date) => date.replace(/T.*$/, '');
+
+/**
+ * Insert a time specification to a date string as described by an HTML "date"
+ * input element so that the string conforms to ISO 8601.
+ *
+ * @param {string}  date
+ *
+ * @returns {string}
+ */
+const expandDate = (date) => date + 'T00:00:00.000Z';
+
 export default function Constraints({
 	onClose, startDate, onStartDateChange, endDate, onEndDateChange, budget,
 	onBudgetChange
@@ -18,16 +38,16 @@ export default function Constraints({
 			<FormField label="Start date" width="50%" paddingRight={1} style={{float: 'left'}}>
 				<Input
 					type="date"
-					value={startDate}
-					onChange={(event) => onStartDateChange(event.target.value)}
+					value={trimDate(startDate)}
+					onChange={(event) => onStartDateChange(expandDate(event.target.value))}
 					/>
 			</FormField>
 
 			<FormField label="End date" width="50%" paddingLeft={1} style={{float: 'left'}}>
 				<Input
 					type="date"
-					value={endDate}
-					onChange={(event) => onEndDateChange(event.target.value)}
+					value={trimDate(endDate)}
+					onChange={(event) => onEndDateChange(expandDate(event.target.value))}
 					/>
 			</FormField>
 
