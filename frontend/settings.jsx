d2150c4c218dd4adb618b2579378bdf866f7d6e3
Mike Pennisi <mike@mikepennisi.com>
2020-06-04 13:29:56 -0400

    Use HTML character code

--- frontend/settings.jsx
+++ frontend/settings.jsx
@@ -241,7 +241,7 @@ export default function Settings({consumerName, producerName, assignmentName}) {
                     style={fieldStyle}>
                     <div style={{'font-size': 'x-small', 'padding-bottom': '1em'}}>
                         Use this to specify additional attributes that are
-                        part of your decision making process, such as 'Cuisine'.
+                        part of your decision making process, such as &apos;Cuisine&apos;.
                     </div>
                     <FieldPickerSynced
                         globalConfigKey="producers:descriptor"
